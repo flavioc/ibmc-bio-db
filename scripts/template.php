e45754dfc6f7c69815c1776956bec5a296ac9cf7
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2008-12-26 00:42:43 +0000

    Add template script.

---
+++ scripts/template.php
@@ -0,0 +1,12 @@
+<?php
+
+$system_folder = "../system";
+require_once('ci_model_remote_open.php');
+require_once(APPPATH . "/models/taxonomy_rank_model.php");
+
+$CI->load->model('taxonomy_model');
+
+require_once('ci_model_remote_close.php');
+
+?> 
+
