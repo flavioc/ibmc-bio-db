f37e78f50933fa0749d846f35e93d1b589cf1527
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-02-18 18:49:03 +0000

    Add more scripts.

---
+++ trunk/bd/users.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+USERNAME=fdb_app
+PASSWORD=$1
+DATABASE=FDB
+HOSTNAME=localhost
+
+mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type, birthday) VALUES('flavio', 'Flavio Cruz', 'ibmc123', 'flaviocruz@gmail.com', 'admin', '11-12-1986')"
+mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type) VALUES('nf', 'Nuno fonseca', 'ibmc123', 'nf@gmail.com', 'admin')"
+
