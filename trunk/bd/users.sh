bd305064d386dd071172e68d4c5196f4233272ef
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-03-02 12:42:54 +0000

    Add jbvieira user.

--- trunk/bd/users.sh
+++ trunk/bd/users.sh
@@ -7,4 +7,5 @@ HOSTNAME=localhost
 
 mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type, birthday) VALUES('flavio', 'Flavio Cruz', 'ibmc123', 'flaviocruz@gmail.com', 'admin', '11-12-1986')"
 mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type) VALUES('nf', 'Nuno fonseca', 'ibmc123', 'nf@gmail.com', 'admin')"
+mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type) VALUES('jbvieira', 'Jorge B. Vieira', 'ibmc123', 'jbvieira@ibmc.up.pt', 'admin')"
 
