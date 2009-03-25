34cdeb59470aa499e795e1179f340de9fde08220
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-03-25 01:17:29 +0000

    Correct birthday.

--- trunk/bd/users.sh
+++ trunk/bd/users.sh
@@ -5,7 +5,7 @@ PASSWORD=$1
 DATABASE=FDB
 HOSTNAME=localhost
 
-mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type, birthday) VALUES('flavio', 'Flavio Cruz', 'ibmc123', 'flaviocruz@gmail.com', 'admin', '11-12-1986')"
+mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type, birthday) VALUES('flavio', 'Flavio Cruz', 'ibmc123', 'flaviocruz@gmail.com', 'admin', '1986-12-11')"
 mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type) VALUES('nf', 'Nuno fonseca', 'ibmc123', 'nf@gmail.com', 'admin')"
 mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO user(name, complete_name, password, email, user_type) VALUES('jbvieira', 'Jorge B. Vieira', 'ibmc123', 'jbvieira@ibmc.up.pt', 'admin')"
 
