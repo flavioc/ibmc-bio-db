51c82fe79d500eb84310ede9ff49ef64859f1225
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-03-25 01:13:36 +0000

    Add valid_code and type label.

--- bd/labels.sh
+++ bd/labels.sh
@@ -14,34 +14,42 @@ function add_label()
 	auto_on_creation=$5
 	auto_on_modification=$6
 	code=$7
-	deletable=$8
-	editable=$9
-	multiple=${10}
+  valid_code=$8
+	deletable=$9
+	editable=${10}
+	multiple=${11}
 
-	mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO label(type, name, autoadd, \`default\`, must_exist, auto_on_creation, auto_on_modification, code, deletable, editable, multiple) VALUES('$type', \"$name\", $autoadd, TRUE, $must_exist, $auto_on_creation, $auto_on_modification, \"$code\", $deletable, $editable, $multiple)"
+	mysql -h $HOSTNAME -u $USERNAME -D $DATABASE --password=$PASSWORD -e "INSERT INTO label(type, name, autoadd, \`default\`, must_exist, auto_on_creation, auto_on_modification, code, valid_code, deletable, editable, multiple) VALUES('$type', \"$name\", $autoadd, TRUE, $must_exist, $auto_on_creation, $auto_on_modification, \"$code\", \"$valid_code\", $deletable, $editable, $multiple)"
 }
 
 add_label "integer" "length" "TRUE" "TRUE" \
 	"TRUE" "TRUE" "return strlen(\$content);" \
+  "return \$data > 0;" \
 	"FALSE" "FALSE" "FALSE"
 
 add_label "ref" "refseq" "FALSE" "FALSE" \
 	"FALSE" "FALSE" "" \
+  "return \$data > 0;" \
 	"TRUE" "TRUE" "FALSE"
 
 add_label "position" "refpos" "FALSE" "FALSE" \
-	"FALSE" "FALSE" "" \
+	"FALSE" "FALSE" "" "" \
 	"TRUE" "TRUE" "FALSE"
 
 add_label "url" "url" "FALSE" "FALSE" \
-	"FALSE" "FALSE" "" \
+	"FALSE" "FALSE" "" "" \
 	"TRUE" "TRUE" "TRUE"
 
 add_label "integer" "internal_id" "TRUE" "TRUE" \
 	"TRUE" "FALSE" "return \$id;" \
+  "return \$data > 0;" \
 	"FALSE" "FALSE" "FALSE"
 
 add_label "bool" "perm_public" "TRUE" "TRUE" \
-  "TRUE" "FALSE" "return false;" \
+  "TRUE" "FALSE" "return false;" "" \
   "FALSE" "TRUE" "FALSE"
 	
+add_label "text" "type" "TRUE" "TRUE" \
+  "TRUE" "TRUE" "return sequence_type(\$content);" \
+  "return \$data == 'dna' || \$data == 'protein';" \
+  "FALSE" "TRUE" "FALSE"
