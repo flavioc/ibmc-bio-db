f2f88a7b174e9faaaf059850081ea7571ecb3475
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-03-25 01:09:11 +0000

    Update script.

--- bd/labels.sh
+++ bd/labels.sh
@@ -41,7 +41,6 @@ add_label "integer" "internal_id" "TRUE" "TRUE" \
 	"TRUE" "FALSE" "return \$id;" \
 	"FALSE" "FALSE" "FALSE"
 
-
 add_label "bool" "perm_public" "TRUE" "TRUE" \
   "TRUE" "FALSE" "return false;" \
   "FALSE" "TRUE" "FALSE"
