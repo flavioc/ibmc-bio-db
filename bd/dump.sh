48346693488e0cc3a20b485ae8e82c471e96620e
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-04-27 14:10:36 +0000

    Remove root user.

--- bd/dump.sh
+++ bd/dump.sh
@@ -3,4 +3,4 @@
 USER=$1
 PASSWORD=$2
 
-mysqldump FDB -R -u $USER --password=$PASSWORD --add-drop-table --no-data > scheme.sql
+mysqldump FDB -R -u $USER --password=$PASSWORD --add-drop-table --no-data | sed 's/`root`/`fdb_app`/g' > scheme.sql
