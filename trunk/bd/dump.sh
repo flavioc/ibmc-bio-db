c968de4363e3b808e501084bc7502ca6b3138218
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-04-27 14:19:01 +0000

    Remove AUTO_INCREMENT.

--- trunk/bd/dump.sh
+++ trunk/bd/dump.sh
@@ -3,4 +3,4 @@
 USER=$1
 PASSWORD=$2
 
-mysqldump FDB -R -u $USER --password=$PASSWORD --add-drop-table --no-data | sed 's/`root`/`fdb_app`/g' > scheme.sql
+mysqldump FDB -R -u $USER --password=$PASSWORD --add-drop-table --no-data | sed 's/`root`/`fdb_app`/g' | sed 's/ AUTO_INCREMENT=[0-9]\+//' > scheme.sql
