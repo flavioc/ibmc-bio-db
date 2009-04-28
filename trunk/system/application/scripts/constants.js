c09fd0c7547f2f5f4022a47ee68f914f548a6fde
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-04-28 00:39:36 +0000

    More constants.

--- trunk/system/application/scripts/constants.js
+++ trunk/system/application/scripts/constants.js
@@ -4,3 +4,7 @@ var w_update = '20%';
 var w_tree = '15%';
 var w_select = '8%';
 var w_rank = '15%';
+var w_del = '15%';
+var w_type = '15%';
+var img_add = build_image_url('add.png', 'image_url');
+var img_del = build_image_url('delete.png', 'image_url');
