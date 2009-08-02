6b0c6351ddf1f73821e7356dd24268a7c13712f2
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-08-02 22:16:31 +0000

    Rename.

---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+echo "***************************"
+echo "Before proceeding please create a mysql database"
+echo "***************************"
+
+echo "What's the site base URL? (something like http://evolution.ibmc.up.pt/~flavio/bio)"
+echo -n "> "
+read SITE_URL
+
+if [ -z "$SITE_URL" ]; then
+	echo "Invalid URL."
+	exit 1
+fi
+
+echo "What's the site location in apache's document root? Example: /home/flavio/public_html/bio"
+echo "In the example, please note that this will only create a symlink bio in public_html."
+echo -n "> "
+read SITE_DIR
+
+if [ -z "$SITE_DIR" ]; then
+	echo "Invalid site location."
+	exit 1
+fi
+
+echo "What's the database name?"
+echo -n "> "
+read DATABASE
+
+if [ -z "$DATABASE" ]; then
+	echo "Invalid database."
+	exit 1
+fi
+
+echo "What's the database username?"
+echo -n "> "
+read USER
+
+if [ -z "$USER" ]; then
+	echo "Invalid username."
+	exit 1
+fi
+
+echo "What's the database password? (password is not echoed)"
+echo -n "> "
+read -s PASSWORD
+
+if [ -z "$PASSWORD" ]; then
+	echo "Invalid password."
+	exit 1
+fi
+
+sh config_site.sh "$SITE_URL" || exit 1
+sh config_fs.sh "$SITE_DIR" || exit 1
+sh config_db.sh "$DATABASE" "$USER" "$PASSWORD" || exit 1
+
+(cd scripts && sh run_all.sh) || exit 1
+
+echo
+echo "**********************"
+echo "Everything's done!"
+echo "To uninstall just do rm -f $SITE_DIR (it will only remove a symlink)"
+echo "Point your browser to $SITE_URL"
+
