fd45fe0679ad9d405153ca777c927236fcb41a15
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-08-02 21:49:21 +0000

    Create config_fs.

---
+++ config_fs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+LOCATION=$1
+NAME=www
+
+if [ -z "$LOCATION" ]; then
+	echo "usage: <apache subdir>"
+	exit 1
+fi
+
+CACHE_DIR=$PWD/system/cache
+UPLOAD_DIR=$PWD/uploads
+
+ln -sf $PWD/www $LOCATION || exit 1
+ln -sf $PWD/system/application/images www/ || exit 1
+ln -sf $PWD/system/application/scripts www/ || exit 1
+ln -sf $PWD/system/application/styles www/ || exit 1
+rm -rf $CACHE_DIR || exit 1
+rm -rf $UPLOAD_DIR || exit 1
+mkdir -p $CACHE_DIR || exit 1
+mkdir -p $UPLOAD_DIR || exit 1
+chmod -R ugo+w $CACHE_DIR || exit 1
+chmod -R ugo+w $UPLOAD_DIR || exit 1
+
+exit 0
