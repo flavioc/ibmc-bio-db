c018a0506910a40ef64e9dc48d3afa514373341c
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-09-04 21:14:37 +0000

    Add Chips library.

---
+++ system/application/libraries/Chips.php
@@ -0,0 +1,48 @@
+<?php
+
+/* Runs the chip EMBOSS utility over a sequence */
+
+class Chips
+{
+  private $path = null;
+  private $output = null;
+  
+  function Chips()
+  {
+    $this->path = find_executable('chips');
+		
+		if(!$this->path) {
+		  throw new Exception('Could not find chips');
+		}
+  }
+  
+  public function run_seq($sequence, $coptions = array())
+  {
+    $options = array('sum' => true,
+                    'sbegin1' => null, 'send1' => null, 'sreverse1' => null,
+                                      'snucleotide1' => null, 'sprotein1' => null);
+    $options = array_merge($options, $coptions);
+    
+    $command = $this->path;
+    $command .= ' -sum ' . parse_yes_r($options['sum']);
+    if($options['sbegin1'] != null) {
+      $command .= ' -sbegin1 ' . $options['sbegin1'];
+    }
+    if($options['send1'] != null) {
+      $command .= ' -send1 ' . $options['send1'];
+    }
+    if($options['sreverse1'] != null) {
+      $command .= ' -sreverse1 ' . parse_yes_r($options['sreverse1']);
+    }
+    if($options['snucleotide1'] != null) {
+      $command .= ' -snucleotide1 ' . parse_yes_r($options['snucleotide1']);
+    }
+    if($options['sprotein1'] != null) {
+      $command .= ' -sprotein1 ' . parse_yes_r($options['sprotein1']);
+    }
+    
+    $this->output = run_util_over_sequence($sequence, $command);
+    
+    return true;
+  }
+}
