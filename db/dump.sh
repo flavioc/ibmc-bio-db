36dd8c8d9bd02ce0cd1cfff18b0d8a9600d0fff9
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-09-06 23:11:33 +0000

    Database param.

--- db/dump.sh
+++ db/dump.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+DATABASE=FDB
 USER=fdb_app
 PASSWORD=$1
 
-mysqldump FDB -R -u $USER --password=$PASSWORD --add-drop-table --no-data -y | sed -e 's/`root`/`fdb_app`/g' -e 's/AUTO_INCREMENT=[0-9]* //g' | sed -e 's/\/\*\![0-9]* DEFINER=\`fdb_app\`\@\`localhost\`\*\/ //g' -e '/\/\*\![0-9]* DEFINER=\`fdb_app\`\@\`localhost\` SQL SECURITY DEFINER \*\//d' -e 's/FDB\.//g' > scheme.sql
+mysqldump $DATABASE -R -u $USER --password=$PASSWORD --add-drop-table --no-data -y | sed -e 's/`root`/`fdb_app`/g' -e 's/AUTO_INCREMENT=[0-9]* //g' | sed -e 's/\/\*\![0-9]* DEFINER=\`fdb_app\`\@\`localhost\`\*\/ //g' -e '/\/\*\![0-9]* DEFINER=\`fdb_app\`\@\`localhost\` SQL SECURITY DEFINER \*\//d' -e 's/FDB\.//g' > scheme.sql
