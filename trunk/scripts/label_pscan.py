cdc558ef10a6b0990e23ba8e94c8eb77f609c32b
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-09-22 20:20:26 +0000

    More pscan labels.

--- trunk/scripts/label_pscan.py
+++ trunk/scripts/label_pscan.py
@@ -11,3 +11,53 @@ add_label(name = "prints_fingerprint",
     code = "$this->load->library('PScan'); $this->pscan->run_seq($content); return $this->pscan->get_prints_fingerprints();",
     deletable = True,
     editable = False)
+
+add_label(name = "prints_fingerprint_elements",
+    type = "integer",
+    multiple = True,
+    must_exist = False,
+    auto_on_creation = False,
+    auto_on_modification = True,
+    code = "$this->load->library('PScan'); $this->pscan->run_seq($content); return $this->pscan->get_prints_fingerprint_elements();",
+    deletable = True,
+    editable = False)
+
+add_label(name = "prints_fingerprint_accession",
+    type = "text",
+    multiple = True,
+    must_exist = False,
+    auto_on_creation = False,
+    auto_on_modification = True,
+    code = "$this->load->library('PScan'); $this->pscan->run_seq($content); return $this->pscan->get_prints_fingerprint_accession();",
+    deletable = True,
+    editable = False)
+
+add_label(name = "prints_fingerprint_threshold",
+    type = "float",
+    multiple = True,
+    must_exist = False,
+    auto_on_creation = False,
+    auto_on_modification = True,
+    code = "$this->load->library('PScan'); $this->pscan->run_seq($content); return $this->pscan->get_prints_fingerprint_thresholds();",
+    deletable = True,
+    editable = False)
+
+add_label(name = "prints_fingerprint_score",
+    type = "float",
+    multiple = True,
+    must_exist = False,
+    auto_on_creation = False,
+    auto_on_modification = True,
+    code = "$this->load->library('PScan'); $this->pscan->run_seq($content); return $this->pscan->get_prints_fingerprint_scores();",
+    deletable = True,
+    editable = False)
+
+add_label(name = "prints_fingerprint_position",
+    type = "position",
+    multiple = True,
+    must_exist = False,
+    auto_on_creation = False,
+    auto_on_modification = True,
+    code = "$this->load->library('PScan'); $this->pscan->run_seq($content); return $this->pscan->get_prints_fingerprint_positions();",
+    deletable = True,
+    editable = False)
