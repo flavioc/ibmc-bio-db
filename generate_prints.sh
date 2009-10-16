885776fde392d4f3e9e3be9fdef172f1e30c9430
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-10-16 21:42:40 +0000

    Better script.

--- generate_prints.sh
+++ generate_prints.sh
@@ -5,6 +5,6 @@ FILE=prints39_0.dat
 wget -c ftp://bioinf.man.ac.uk/pub/prints/$FILE.gz || exit 1
 gzip -d $FILE.gz || exit 1
 
-printsextract $FILE
+printsextract $FILE || exit 1
 
-rm -f $FILE || exit 1
+rm -f $FILE
