b96504afcbcb4e18b5ae639da9f62e860fdb65ad
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2009-11-07 20:57:22 +0000

    add js file.

--- config_site.sh
+++ config_site.sh
@@ -7,6 +7,13 @@ if [ -z "$SITE" ]; then
 	exit 1
 fi
 
+get_cookie_path()
+{
+  echo $* | gawk -F'(http://[^/]+|?)' '$0=$2'
+}
+
+cookie_path=$(get_cookie_path $SITE)
 sed -e "s@SITE_BASE@$SITE@" config/general.in > application/config/config.php
+echo "var cookie_path = '$cookie_path';" > application/scripts/cookies.js
 
 exit 0
