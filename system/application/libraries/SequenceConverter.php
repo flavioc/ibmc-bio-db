461ab8d5ec15d7062914c98ea54ff866758f5a1c
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2010-02-23 16:08:30 +0000

    add code to convert a dna fasta file to protein file keeping its label structure

--- system/application/libraries/SequenceConverter.php
+++ system/application/libraries/SequenceConverter.php
@@ -49,4 +49,53 @@ class SequenceConverter
     
     return $this->convert_dna_sequences($data);
   }
+  
+  private function __get_fasta_header($file)
+  {
+    $CI =& get_instance();
+    $CI->load->plugin('line_reader');
+
+    $reader = new LineReader($file);
+    
+    $line = $reader->get_line();
+    
+    if($line[0] == '#')
+      return $line;
+    return '';
+  }
+  
+  public function convert_dna_protein($dna_file)
+  {
+    $protein_file = generate_new_file_name();
+    
+    $header = $this->__get_fasta_header($dna_file);
+    
+    # remove header line
+    $dna_file2 = generate_new_file_name();
+    $sed = find_executable("sed");
+    $cmd = "$sed -e '/#.*/d' $dna_file > $dna_file2";
+    $do_sed = shell_exec($cmd);
+    if($do_sed) {
+      unlink($dna_file2);
+      return null;
+    }
+    
+    $protein_file = $this->convert_dna_fasta($dna_file2);
+    unlink($dna_file2);
+    
+    # put header back
+    $new_protein = write_raw_file($header . "\n");
+    shell_exec("cat $protein_file >> $new_protein");
+    shell_exec("mv $new_protein $protein_file");
+    
+    # convert protein names
+    $cmd = "$sed -e 's/_[0-9]/_p/g' -i '' $protein_file";
+    $do_sed = shell_exec($cmd);
+    if($do_sed) {
+      unlink($protein_file);
+      return null;
+    }
+    
+    return $protein_file;
+  }
 }
