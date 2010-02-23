2a6a803339192f9c7d47b8de80fe765252fdf62d
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2010-02-23 16:15:22 +0000

    add keep structure option

--- trunk/tools/import_and_generate.sh
+++ trunk/tools/import_and_generate.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+# 
+#
 
 FILE=$1
+KEEP_STRUCTURE=$2
 
-if [ -z "$FILE" ]; then
-  echo "usage: <dna fasta or xml file>"
+if test -z "$FILE" || test -z "$KEEP_STRUCTURE"; then
+  echo "usage: <dna fasta or xml file> <keep structure: yes or no>"
   exit 1
 fi
 
-php wrapper.php command_line import_sequence_file_and_generate "$FILE"
+php wrapper.php command_line import_sequence_file_and_generate "$FILE" "$KEEP_STRUCTURE"
