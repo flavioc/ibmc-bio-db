8514cc529060e7b86f2a3bfcca9d5e5f5bbd49f4
flaviocruz <flaviocruz@e81089b0-c564-11dd-b192-8d970c37f332>
2010-03-08 16:48:10 +0000

    get latest prints database

--- generate_prints.sh
+++ generate_prints.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-FILE=prints39_0.dat
+URL=ftp://bioinf.man.ac.uk/pub/prints
+VERSIONFILE=newpr.lis.gz
 
-wget -c ftp://bioinf.man.ac.uk/pub/prints/$FILE.gz || exit 1
-gzip -d $FILE.gz || exit 1
+function discover_version ()
+{
+  rm -f $VERSIONFILE
+  wget -q $URL/$VERSIONFILE || exit 1
+  gunzip -c $VERSIONFILE | grep VERSION | awk -F' ' {'print $3'} | sed -e 's/\./_/'
+  rm -f $VERSIONFILE
+}
+
+VERSION=`discover_version`
+FILE=prints$VERSION.dat
+echo "Last version is $VERSION..."
+wget -c $URL/$FILE.gz || exit 1
+gzip -fd $FILE.gz || exit 1
 
 printsextract $FILE || exit 1
 
 rm -f $FILE
+
+exit 0
